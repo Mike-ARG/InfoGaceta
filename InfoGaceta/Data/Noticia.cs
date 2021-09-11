53cf247145faa862a9b6cb65c279406e448b4e48
Miguel <milito_vanetta@hotmail.com>
2021-09-11 18:20:14 -0300

    Añadida clase "Noticia". Se inicia proceso para modificar el programa a fin de utilizar Orientación a Objetos y no depender de una estructura de tipo Diccionario, la cuál es muy limitada y poco escalable en funcionalidad.

---
+++ InfoGaceta/Data/Noticia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfoGaceta.Data
+{
+    public class Noticia
+    {
+        //Esta clase almacenará los datos referidos a las noticias que se extraerán del sitio.
+        public string Titulo { get; set; }
+        public string Categoria { get; set; }
+        public string Link { get; set; }
+
+    }
+}
